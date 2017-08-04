278dbafed1b7c040fbe5934bfdab14030fb4506a
Andreas Lonberg <andreas.lonberg@gmail.com>
2017-08-04 17:26:15 +0100

    Changed timezone

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+
 var time = require('time');
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.date();
-    currentTime.setTimezone("Europe/London");
+    var currentTime = new time.Date(); 
+    currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in London is: ' + currentTime.toString(),
-    })
-
+        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+    });
 };
